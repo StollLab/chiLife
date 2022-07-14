b6a0b114c6d3741fac4e3fda817bde6c1660e48b
Maxx Tessmer <mhtessmer@gmail.com>
2022-07-14 15:15:28 -0700

    base implementation of Protein and AtomSelection objects

--- chiLife/Protein.py
+++ chiLife/Protein.py
@@ -1,79 +1,226 @@
+import operator
 from itertools import groupby
+import parse
 import numpy as np
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class Protein:
     def __init__(
-        self, coords, atom_names, atom_types, atom_residues, atom_chain, res_names
+        self,
+        atomids: np.ndarray,
+        names: np.ndarray,
+        altlocs: np.ndarray,
+        resnames: np.ndarray,
+        resnums: np.ndarray,
+        chains: np.ndarray,
+        coords: np.ndarray,
+        occupancies: np.ndarray,
+        bs: np.ndarray,
+        atypes: np.ndarray,
+        charges: np.ndarray,
     ):
-        self.coords = coords
-        self.atom_names = atom_names
-        self.atom_types = atom_types
-        self.atom_residues = atom_residues
-        self.atom_chain = atom_chain
-        self.res_names = res_names
+
+        self.atomids = atomids.copy()
+        self.names = names.copy()
+        self.altlocs = altlocs.copy()
+        self.resnames = resnames.copy()
+        self.resnums = resnums.copy()
+        self.chains = chains.copy()
+        self.coords = coords.copy()
+        self.occupancies = occupancies.copy()
+        self.bs = bs.copy()
+        self.atypes = atypes.copy()
+        self.charges = charges.copy()
+
+        self.n_atoms = len(self.atomids)
+        self.n_residues = len(np.unique(self.resnums))
+        self.n_chains = len(np.unique(self.chains))
+
+        self.protein_keywords = {'id': self.atomids,
+                                 'name': self.names,
+                                 'altloc': self.altlocs,
+                                 'resname': self.resnames,
+                                 'resnum': self.resnums,
+                                 'chain': self.chains,
+                                 'occupancies': self.occupancies,
+                                 'b': self.bs,
+                                 'type': self.atypes,
+                                 'charges': self.charges,
+                                 '_len': self.n_atoms}
+
+
+
+    def select_atoms(self, selstr):
+        mask = process_statement(selstr, logic_keywords, self.protein_keywords)
+        return AtomSelection(self, mask)
 
     @classmethod
     def from_pdb(cls, file_name):
-        atom_names = None
-        # Read file
-        with open(file_name, "r") as f:
+        """reads a pdb file and returns a Protein object"""
+
+        fmt_str = "{:6.6S}{:5.5d} {:4.4S}{:1.1}{:3.3} {:1.1S}{:4.4d}{:1.1}   " \
+                  "{:8.8f}{:8.8f}{:8.8f}{:6.6f}{:6.6f}          " \
+                  "{:2.2S}{:2.2}\n"
+
+        keys = ["skip", "atomids", "names", "altlocs", "resnames", "chains", "resnums",
+                "skip", "x", "y", "z", "occupancies", "bs", "atypes", "charges"]
+
+        with open(file_name, 'r') as f:
             lines = f.readlines()
 
-        # group by model
-        models = groupby(lines, lambda x: x.startswith("MODEL"))
+        PDB_data = [parse.parse(fmt_str, line) for line in lines if line.startswith(('ATOM', 'HETATM'))]
+        pdb_dict = {key: np.array(data) for key, data in zip(keys, zip(*PDB_data)) if key != "skip"}
 
-        coords = []
-        # loop over models and extract information
-        for key, model in models:
+        pdb_dict['coords'] = np.vstack([pdb_dict.pop(xyz) for xyz in 'xyz']).T
 
-            model = [line.strip().split() for line in model if line.startswith("ATOM")]
+        return cls(**pdb_dict)
 
-            for line in model:
-                if len(line) != 13:
-                    line.insert(3, line[2][4:])
-                    line[2] = line[2][:3]
 
-            model = np.array(model)
 
-            if model.size == 0:
+logic_keywords = {'and': operator.mul,
+                  'or': operator.add,
+                  'not': operator.sub,
+                  '!': operator.sub,
+                  '<': operator.lt,
+                  '>': operator.gt,
+                  '==': operator.eq,
+                  '<=': operator.le,
+                  '>=': operator.ge,
+                  "!=": operator.ne}
+
+def process_statement(statement, logickws, subjectkws):
+    sub_statements = parse_paren(statement)
+
+    mask = np.ones(subjectkws['_len'], dtype=bool)
+    operation = logickws['and']
+    next_operation = None
+    for stat in sub_statements:
+        cont = False
+        stat_split = stat.split()
+
+        if stat_split[0] in logickws:
+
+            if operation != None:
+                raise ValueError('Cannot have two logical operators in a row')
+
+            operation = logickws[stat_split[0]]
+            if len(stat_split) == 1:
                 continue
 
-            coords.append(model[:, 6:9].astype(float))
+            stat_split = stat_split[1:]
 
-            if atom_names is None:
-                atom_names = model[:, 2]
-                atom_types = model[:, 12]
-                atom_resnums = model[:, 5].astype(int)
-                res_names = model[:, 3]
-                atom_chain = model[:, 4]
-            else:
-                assert np.all(atom_names == model[:, 2])
-                assert np.all(atom_types == model[:, 12])
-                assert np.all(atom_resnums == model[:, 5].astype(int))
-                assert np.all(res_names == model[:, 3])
-                assert np.all(atom_chain == model[:, 4])
-        coords = np.array(coords)
+        elif stat.startswith('('):
+            cont = True
+            sub_out = process_statement(stat, logickws, subjectkws)
 
-        return cls(coords, atom_names, atom_types, atom_resnums, atom_chain, res_names)
+        if stat_split[0] not in subjectkws and not cont:
+            raise ValueError(f"{stat_split[0]} is not a valid keyword. Please start expressions with valid keywords")
 
-    # def get_coords(self, selstring):
-    #     idxs = _parse_selstring()
-    #
-    # def _parse_selstring(self, selstring):
-    #     sel = selstring.split()
-    #
-    #     # Group logical operators
-    #     groupby(sel, lambda x: x in ['resnum, resname, name'])
+
+        if stat_split[0] in subjectkws:
+            finished = False
+            sub_out = np.ones(subjectkws['_len'], dtype=bool)
+            internal_operation = logickws['and']
+            next_internal_operation = None
+            while not finished:
+                if stat_split[0] in subjectkws:
+                    subject = subjectkws[stat_split.pop(0)]
+                    values = []
+
+                elif stat_split[0] in logickws:
+                    if logickws[stat_split[0]] is operator.sub:
+                        dummy = internal_operation
+                        internal_operation = lambda a, b: dummy(a, ~b)
+                        stat_split = stat_split[1:]
+                        continue
+                    else:
+                        raise ValueError('Cannot have two logical operators in succession unless the second one is '
+                                        '`not`')
+                else:
+                    raise ValueError(f'{stat_split[0]} is not a valid keyword')
+
+                for i, val in enumerate(stat_split):
+                    if val in logickws:
+                        next_internal_operation = logickws[val]
+                        stat_split = stat_split[i+1:]
+                        if stat_split == []:
+                            next_operation = next_internal_operation
+                            next_internal_operation = None
+                        break
+                    elif '-' in val:
+                        start, stop = [int(x) for x in val.split('-')]
+                        values += list(range(start, stop+1))
+                    elif ':' in val:
+                        start, stop = [int(x) for x in val.split(':')]
+                        values += list(range(start, stop+1))
+                    else:
+                        values.append(val)
+                else:
+                    finished = True
+
+                values = np.array(values, dtype=subject.dtype)
+                tmp = np.isin(subject, values)
+                sub_out = internal_operation(sub_out, tmp)
+
+
+                internal_operation = next_internal_operation
+                next_internal_operation = None
+                if internal_operation is None:
+                    finished = True
+
+        if operation is None:
+            raise ValueError('Need an operation between two selections')
+
+        mask = operation(mask, sub_out)
+        operation = next_operation
+        next_operation = None
+
+
+    return mask
+
+def parse_paren(string):
+    stack = 0
+    start_idx = 0
+    end_idx = 0
+    results = []
+
+
+    for i, c in enumerate(string):
+        if c == '(':
+            if stack == 0:
+                start_idx = i + 1
+                if i != 0:
+                    results.append(string[end_idx:i].strip())
+            stack += 1
+        elif c == ')':
+            # pop stack
+            stack -= 1
+
+            if stack == 0:
+                results.append(f'({string[start_idx:i].strip()})')
+                start_idx = end_idx
+                end_idx = i + 1
+
+
+    if end_idx != len(string) - 1:
+        results.append(string[end_idx:].strip())
+
+    results = [res for res in results if res != '']
+    if len(results) == 1 and results[0].startswith('('):
+        results = parse_paren(results[0][1:-1])
+
+    return results
+
+
+class AtomSelection:
+
+    def __init__(self, protein, mask):
+        self.protein = protein
+        self.mask = mask
+        prot_dict = {kw: self.protein.__dict__[kw][self.mask] for kw in
+                     ("atomids", "names", "altlocs", "resnames", "chains", "resnums",
+                      "coords", "occupancies", "bs", "atypes", "charges")}
+        self.__dict__.update(prot_dict)
+
+    def select_atoms(self, selstr):
+        output = process_statement(selstr, logic_keywords, self.protein_keywords)
+        return output
---
+++ tests/test_Protein.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pytest
+
+from chiLife.Protein import Protein, parse_paren
+prot = Protein.from_pdb('test_data/1omp_H.pdb')
+
+def test_from_pdb():
+    ans = np.array([[  0.522, -23.214, -22.149],
+                    [ -1.924, -20.646, -23.898],
+                    [ -1.825, -19.383, -24.623],
+                    [ -1.514, -19.5,   -26.1  ],
+                    [ -2.237, -20.211, -26.799]])
+
+    np.testing.assert_almost_equal(prot.coords[20:25], ans)
+
+
+def test_parse_paren1():
+    P = parse_paren('(name CA or name CB)')
+    assert P == ['name CA or name CB']
+
+def test_parse_paren2():
+    P = parse_paren('(resnum 32 and name CA) or (resnum 33 and name CB)')
+    assert len(P) == 3
+
+def test_parse_paren3():
+    P = parse_paren('resnum 32 and name CA or (resnum 33 and name CB)')
+    assert len(P) == 2
+
+def test_parse_paren4():
+    P = parse_paren('resname LYS ARG PRO and (name CA or (name CB and resname PRO)) or resnum 5')
+    assert len(P) == 3
+
+def test_parse_paren5():
+    P = parse_paren('(name CA or (name CB and resname PRO) or name CD)')
+    assert len(P) == 3
+
+def test_select_or():
+    m1 = prot.select_atoms('name CA or name CB')
+    ans_mask = (prot.names == 'CA') + (prot.names == 'CB')
+
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    assert np.all(np.isin(m1.names, ['CA', 'CB']))
+    assert not np.any(np.isin(prot.names[~m1.mask], ['CA', 'CB']))
+
+def test_select_and_or_and():
+
+    m1 = prot.select_atoms('(resnum 32 and name CA) or (resnum 33 and name CB)')
+    ans_mask = (prot.resnums == 32) * (prot.names == 'CA') + (prot.resnums == 33) * (prot.names == 'CB')
+
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    assert np.all(np.isin(m1.names, ['CA', 'CB']))
+    assert np.all(np.isin(m1.resnums, [32, 33]))
+
+
+def test_select_and_not():
+    m1 = prot.select_atoms('resnum 32 and not name CA')
+    ans_mask = (prot.resnums == 32) * (prot.names != 'CA')
+
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    assert not np.any(np.isin(m1.names, ['CA']))
+    assert np.all(np.isin(m1.resnums, [32]))
+
+
+def test_select_name_and_resname():
+    m1 = prot.select_atoms("name CB and resname PRO")
+    ans_mask = (prot.names == 'CB') * (prot.resnames == 'PRO')
+
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    assert np.all(np.isin(m1.names, ['CB']))
+    assert np.all(np.isin(m1.resnames, ['PRO']))
+
+
+def test_select_complex():
+    m1 = prot.select_atoms('resname LYS ARG PRO and (name CA or (type C and resname PRO)) or resnum 5')
+
+    resnames = np.isin(prot.resnames, ['LYS', 'ARG', 'PRO'])
+    ca_or_c_pro = ((prot.names == 'CA') + ((prot.atypes == 'C') * (prot.resnames == 'PRO' )))
+    resnum5 = (prot.resnums == 5)
+    ans_mask = resnames * ca_or_c_pro + resnum5
+
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    assert not np.any(np.isin(prot.resnums[~m1.mask], 5))
+
+
+def test_select_range():
+    m1 = prot.select_atoms('resnum 5-15')
+    m2 = prot.select_atoms('resnum 5:15')
+    ans_mask = np.isin(prot.resnums, list(range(5, 16)))
+    np.testing.assert_almost_equal(m1.mask, ans_mask)
+    np.testing.assert_almost_equal(m2.mask, ans_mask)
+
+@pytest.mark.parameterize(features, 'feature')
+def test_AtomSelection_features(feature)
+
+

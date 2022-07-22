b2306f2bc1e38e8ec3efc52e3b0a5a58da44ff0c
Maxx Tessmer <mhtessmer@gmail.com>
2022-07-21 19:35:20 -0700

    lower tolerance of test to account for differences between MKL and OpenBlas

--- tests/test_SpinLabel.py
+++ tests/test_SpinLabel.py
@@ -119,7 +119,7 @@ def test_from_wizard():
 
     np.testing.assert_almost_equal(SL.coords, SLans.coords, decimal=5)
     np.testing.assert_almost_equal(SL.weights, SLans.weights)
-    np.testing.assert_almost_equal(SL.dihedrals, SLans.dihedrals)
+    np.testing.assert_almost_equal(SL.dihedrals, SLans.dihedrals, decimal=5)
     assert len(SL) == len(SL.dihedrals) == len(SL.weights) == len(SL.coords)
 
 

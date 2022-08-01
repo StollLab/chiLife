a0a72af6f363e26b694fa47cf5f7b576107d161b
Maxx Tessmer <mhtessmer@gmail.com>
2022-08-01 11:14:46 -0700

    Updated some test_data

---
+++ tests/test_Offline.py
@@ -0,0 +1,31 @@
+import numpy as np
+import chiLife as xl
+
+def test_add_dlabel2():
+    Energies = np.loadtxt('test_data/HCS.energies')
+
+    P = np.exp(-Energies/ (xl.GAS_CONST * 278))
+    P /= P.sum()
+
+    dihedral_atoms = [[["N", "CA", "CB", "CG"], ["CA", "CB", "CG", "ND1"]],
+                      [["N", "CA", "CB", "CG"], ["CA", "CB", "CG", "ND1"]]]
+
+    xl.add_dlabel('HCS', 'test_data/HCS.pdb', resi=2, increment=2, dihedral_atoms=dihedral_atoms, spin_atoms='Cu1')
+
+
+def test_add_dlabel():
+    Energies = np.loadtxt("test_data/DHC.energies")[:, 1]
+    P = np.exp(-Energies / (xl.GAS_CONST * 298))
+    P /= P.sum()
+    xl.add_dlabel(
+        "DHC",
+        "test_data/DHC.pdb",
+        4,
+        resi=2,
+        weights=P,
+        dihedral_atoms=[
+            [["N", "CA", "CB", "CG"], ["CA", "CB", "CG", "ND1"]],
+            [["N", "CA", "CB", "CG"], ["CA", "CB", "CG", "ND1"]],
+        ],
+        spin_atoms=["Cu1"],
+    )

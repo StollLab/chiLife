6cb3a3cdb1e3a7bf7e212250e38b9709713ed9d1
Maxx Tessmer <mhtessmer@gmail.com>
2022-11-08 15:39:14 -0800

    Update docstrings, minor additions to fit_superimposition method

--- chiLife/superimpositions.py
+++ chiLife/superimpositions.py
@@ -1,32 +1,44 @@
 import numpy as np
-
+from scipy.linalg import sqrtm
 superimpositions = {}
 
 
 def superimposition(func):
-    """Decorator to add function to the superimpositions dictionary"""
+    """Decorator to add function to the superimpositions dictionary
+
+    Parameters
+    ----------
+    func : callable
+        Function that performs a superimposition.
+
+    Returns
+    -------
+        Unmodified original function.
+    """
     superimpositions[func.__name__.split("_")[0]] = func
     return func
 
 
 @superimposition
 def rosetta_superimposition(N, CA, C):
-    """
-        Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
-    global coordinate frame for a given residue with backbone coordinates (N, C, CA). Principal acis is the C->Ca Bond.
+    """Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
+    global coordinate frame for a given residue with backbone coordinates (N, C, CA). Principal axis is the C->Ca Bond.
 
-    :param N: numpy ndarray (1x3)
-        Backbone nitrogen coordinates
+    Parameters
+    ----------
+    N : numpy.ndarray (1x3)
+        Backbone nitrogen coordinates.
+    CA : numpy.ndarray (1x3)
+        Backbone Calpha carbon coordinates.
+    C : numpy.ndarray (1x3)
+        Backbone carbonyl carbon coordinates.
 
-    :param CA: numpy ndarray (1x3)
-        Backbone Calpha carbon coordinates
-
-    :param C: numpy ndarray (1x3)
-        Backbone carbonyl carbon coordinates
-
-    :return (rotation_matrix, origin) : (numpy ndarray (1x3), numpy ndarray (3x3))
-        rotation_matrix: rotation  matrix to rotate spin label to
-        origin: new origin position in 3 dimensional space
+    Returns
+    -------
+    rotation_matrix : numpy ndarray (3x3)
+        Rotation  matrix to rotate spin label to achieve the correct orientation.
+    origin : numpy.ndarray (1x3)
+        New origin position in 3 dimensional space.
     """
     # Define new Z axis from  C-->CA
     zaxis = CA - C
@@ -49,23 +61,25 @@ def rosetta_superimposition(N, CA, C):
 
 @superimposition
 def bisect_superimposition(N, CA, C):
-    """
-        Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
+    """Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
     global coordinate frame for a given residue with backbone coordinates (N, C, CA). Principal axis bisects the
     N->Ca->C angle.
 
-    :param N: numpy ndarray (1x3)
-        Backbone nitrogen coordinates
+    Parameters
+    ----------
+    N : numpy.ndarray (1x3)
+        Backbone nitrogen coordinates.
+    CA : numpy.ndarray (1x3)
+        Backbone Calpha carbon coordinates.
+    C : numpy.ndarray (1x3)
+        Backbone carbonyl carbon coordinates.
 
-    :param CA: numpy ndarray (1x3)
-        Backbone Calpha carbon coordinates
-
-    :param C: numpy ndarray (1x3)
-        Backbone carbonyl carbon coordinates
-
-    :return (rotation_matrix, origin) : (numpy ndarray (1x3), numpy ndarray (3x3))
-        rotation_matrix: rotation  matrix to rotate spin label to
-        origin: new origin position in 3 dimensional space
+    Returns
+    -------
+    rotation_matrix : numpy ndarray (3x3)
+        Rotation  matrix to rotate spin label to achieve the correct orientation.
+    origin : numpy.ndarray (1x3)
+        New origin position in 3 dimensional space.
     """
     # Define new Z axis that bisects N<--CA-->C angle
     CA_N = N - CA
@@ -92,23 +106,25 @@ def bisect_superimposition(N, CA, C):
 
 @superimposition
 def mmm_superimposition(N, CA, C):
-    """
-    Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
+    """Calculates a rotation matrix and translation to transform an amino acid from the local coordinate frame to the
     global coordinate frame for a given residue with backbone coordinates (N, C, CA). Principal axis is defined
     along the CA->N bond.
 
-    :param N: numpy ndarray (1x3)
-        Backbone nitrogen coordinates
+    Parameters
+    ----------
+    N : numpy.ndarray (1x3)
+        Backbone nitrogen coordinates.
+    CA : numpy.ndarray (1x3)
+        Backbone Calpha carbon coordinates.
+    C : numpy.ndarray (1x3)
+        Backbone carbonyl carbon coordinates.
 
-    :param CA: numpy ndarray (1x3)
-        Backbone Calpha carbon coordinates
-
-    :param C: numpy ndarray (1x3)
-        Backbone carbonyl carbon coordinates
-
-    :return (rotation_matrix, origin) : (numpy ndarray (1x3), numpy ndarray (3x3))
-        rotation_matrix: rotation  matrix to rotate spin label to
-        origin: new origin position in 3 dimensional space
+    Returns
+    -------
+    rotation_matrix : numpy ndarray (3x3)
+        Rotation  matrix to rotate spin label to achieve the correct orientation.
+    origin : numpy.ndarray (1x3)
+        New origin position in 3 dimensional space.
     """
     xaxis = N - CA
     xaxis /= np.linalg.norm(xaxis)
@@ -129,12 +145,36 @@ def mmm_superimposition(N, CA, C):
 
 @superimposition
 def fit_superimposition(N, CA, C):
-    inp = np.array([N, CA, C])
+    """Superimpose the residues such that the root mean squared deviation of the backbone atoms is minimized.
+
+    Parameters
+    ----------
+    N : numpy.ndarray (2x3)
+        Backbone nitrogen coordinates.
+    CA : numpy.ndarray (2x3)
+        Backbone Calpha carbon coordinates.
+    C : numpy.ndarray (2x3)
+        Backbone carbonyl carbon coordinates.
+
+    Returns
+    -------
+    rotation_matrix : numpy ndarray (3x3)
+        Rotation  matrix to rotate spin label to achieve the correct orientation.
+    origin : numpy.ndarray (1x3)
+        New origin position in 3 dimensional space.
+    """
 
     # Check if input is transposed
-    for ar in inp:
+    lst = []
+    for ar in (N, CA, C):
         if ar.shape[1] == 2 and ar.shape[0] == 3:
             ar = ar.T
+        lst.append(ar)
+
+    N, CA, C = lst
+
+    inp0 = np.array([N[0], CA[0], C[0]])
+    target0 = np.array([N[1], CA[1], C[1]])
 
     # Check if input is the appropriate shape
     if len(N) != len(CA) != len(C) != 2:
@@ -143,26 +183,55 @@ def fit_superimposition(N, CA, C):
             "mobile residue and the second coordinate should be from the target residue."
         )
 
+    origin = np.mean(inp0, axis=0)
+
+    inp = inp0 - origin
+    target = target0 - np.mean(target0, axis=0)
+
+    inp_normal = np.cross(inp[0]-inp[1], inp[2] - inp[1])
+    inp_normal /= np.linalg.norm(inp_normal)
+    inp = np.vstack([inp, inp_normal])
+
+    target_normal = np.cross(target[0]-target[1], target[2]-target[1])
+    target_normal /= np.linalg.norm(target_normal)
+    target = np.vstack([target, target_normal])
+
+    H = inp.T @ target
+    U, S, V = np.linalg.svd(H)
+    d = np.sign(np.linalg.det(V @ U.T))
+    new_S = np.eye(3)
+    new_S[2, 2] = d
+    rotation_matrix = V @ U.T
+
     raise NotImplementedError(
         "Superimposition by minimizing RMSD of the backbone coordinates it not yet implemented"
     )
 
 
-def parse_backbone(rotamer_library, kind):
-    """
-    Extract appropriate backbone information to make a rotation matrix using the method provided
+    return rotation_matrix, origin
 
-    :param rotamer_library: RotamerLibrary
+
+
+def parse_backbone(rotamer_library, kind):
+    """Extract appropriate backbone information to make a rotation matrix using the method provided
+
+    Parameters
+    ----------
+    rotamer_library : RotamerLibrary
         RotamerLibrary that the rotation matrix will operate on. If using the `fit` method, the rotamer library must
         have a `protein` feature.
+    kind : str
+        Specifies if the backbone is for the rotamer library (local) or the protein (global)
 
-    :return N, CA, C: tuple
+    Returns
+    -------
+    N, CA, C: tuple
         Numpy arrays of N, CA and C coordinates of the rotamer library backbone. If using method `fit` arrays are 2x3
         with the first coordinate as the rotamer library backbone and the second as the protein site backbone.
     """
     method = rotamer_library.superimposition_method
 
-    if method in {"fit"}:
+    if method.__name__ == "fit_superimposition":
         N1, CA1, C1 = rotamer_library.backbone
         N2, CA2, C2 = rotamer_library.protein.select_atoms(
             f"segid {rotamer_library.chain} and "

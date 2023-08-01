6b6969a4d25dd830daed260eb763ab624656cfe6
Maxx Tessmer <mhtessmer@gmail.com>
2023-08-01 13:36:33 -0700

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PostInstall(install):
 
 setup(
     name='chilife',
-    version='0.1.10-1',
+    version='0.1.10-2',
     python_requires='>=3.8',
     packages=['chilife'],
     package_data={'chilife': ['data/*', 'data/*/*', 'data/*/*/*']},

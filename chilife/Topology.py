50fd8f53da2740668277c1077ffac49ceaf6e2ba
Maxx Tessmer <mhtessmer@gmail.com>
2023-09-12 17:58:19 -0700

    Add features to Topology object

--- chilife/Topology.py
+++ chilife/Topology.py
@@ -15,7 +15,46 @@ class Topology:
         self.angles = kwargs.get('angles', get_angle_defs(self.graph))
         self.dihedrals = kwargs.get('dihedrals', get_dihedral_defs(self.graph))
 
-        # self.dihedrals = kwargs.get('dihedrals', get_dihedral_defs(self.graph))
+        self.dihedrals_by_bonds = {}
+        self.dihedrals_by_atoms = {}
+
+        for dihe in self.dihedrals:
+            b, c, e = dihe[1:]
+            if (b, c) not in self.dihedrals_by_bonds:
+                self.dihedrals_by_bonds[b, c] = [dihe]
+            else:
+                self.dihedrals_by_bonds[b, c].append(dihe)
+
+            if e not in self.dihedrals_by_atoms:
+                self.dihedrals_by_atoms[e] = [dihe]
+            else:
+                self.dihedrals_by_atoms[e].append(dihe)
+
+    def get_zmatrix_dihedrals(self):
+        zmatrix_dihedrals = []
+        hold = []
+        for key in self.atoms:
+            if key not in self.dihedrals_by_atoms:
+                hold.append(key)
+                continue
+
+            if len(hold) > 0:
+                for i, elem in enumerate(hold):
+                    zmatrix_dihedrals.append(hold[:i + 1])
+                hold = []
+            runner_ups = []
+            for dihe in self.dihedrals_by_atoms[key]:
+                if dihe[0] < dihe[1] < dihe[2] < dihe[3]:
+                    zmatrix_dihedrals.append(list(dihe))
+                    break
+                elif max(dihe[:3]) < dihe[3]:
+                    runner_ups.append(dihe)
+            else:
+                if len(runner_ups) > 0:
+                    zmatrix_dihedrals.append(list(runner_ups[0]))
+
+        return zmatrix_dihedrals
+
 
     def _make_graph(self):
         return ig.Graph(n=len(self.atoms), edges=self.bonds)
@@ -50,8 +89,10 @@ def get_dihedral_defs(graph):
 
         if len(a_neighbors) > 1 and len(b_neighbors) > 1:
 
-            bond_dihedrals = [(min(c), a, b, max(c)) for c in product(a_neighbors, b_neighbors)
-                                                  if all(idx not in c for idx in (a, b))]
+            bond_dihedrals = [(aa, a, b, bb) if aa < bb else (bb, b, a, aa)
+                                             for aa, bb in product(a_neighbors, b_neighbors)
+                                             if all(idx not in (aa, bb) for idx in (a, b))]
+
             dihedrals += [tuple(a) for a in bond_dihedrals]
 
     return tuple(dihedrals)
--- tests/test_Topology.py
+++ tests/test_Topology.py
@@ -6,27 +6,24 @@ from MDAnalysis.topology.guessers import guess_angles, guess_dihedrals
 
 ubq = xl.Protein.from_pdb('test_data/1ubq.pdb')
 ubqu = MDAnalysis.Universe('test_data/1ubq.pdb')
-
-
+bonds = xl.guess_bonds(ubq.coords, ubq.atypes)
+ubqu.add_bonds(bonds)
+graph1 = ig.Graph(n = len(ubq.atoms), edges=bonds)
 
 def test_get_angle_defs():
-
-    bonds = xl.guess_bonds(ubq.coords, ubq.atypes)
-    ubqu.add_bonds(bonds)
-
-    graph = ig.Graph(n = len(ubq.atoms), edges=bonds)
-    angles = set(get_angle_defs(graph))
+    angles = set(get_angle_defs(graph1))
     angles2 = set(guess_angles(ubqu.bonds))
 
     assert angles == angles2
 
 
 def test_get_dihedral_defs():
-    bonds = xl.guess_bonds(ubq.coords, ubq.atypes)
-    ubqu.add_bonds(bonds)
-
-    graph = ig.Graph(n=len(ubq.atoms), edges=bonds)
-    dihedrals = set(get_dihedral_defs(graph))
+    dihedrals = set(get_dihedral_defs(graph1))
     dihedrals2 = set(guess_dihedrals(ubqu.bonds))
 
     assert dihedrals == dihedrals2
+
+
+def test_construction():
+    top = Topology(ubq.atoms.ix, bonds)
+    pass

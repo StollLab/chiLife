1ed3302e721d7a4c9f1acc24a36d153601b56b1b
Maxx Tessmer <mhtessmer@gmail.com>
2023-10-24 15:25:29 -0700

    Add mbp to angle and dihedral tests

--- tests/test_Topology.py
+++ tests/test_Topology.py
@@ -1,4 +1,5 @@
 import pickle
+import pytest
 import MDAnalysis
 import chilife as xl
 from chilife.Topology import *
@@ -6,28 +7,38 @@ from MDAnalysis.topology.guessers import guess_angles, guess_dihedrals
 
 ubq = xl.Protein.from_pdb('test_data/1ubq.pdb', sort_atoms=True)
 mbp = MDAnalysis.Universe("test_data/2klf.pdb", in_memory=True)
-
 ubqu = MDAnalysis.Universe('test_data/1ubq.pdb')
-bonds = xl.guess_bonds(ubq.coords, ubq.atypes)
-ubqu.add_bonds(bonds)
-graph1 = ig.Graph(n=len(ubq.atoms), edges=bonds)
 
-def test_get_angle_defs():
+
+
+
+@pytest.mark.parametrize('prot', [ubqu, mbp])
+def test_get_angle_defs(prot):
+    bonds = xl.guess_bonds(prot.atoms.positions, prot.atoms.types)
+    prot.add_bonds(bonds)
+    graph1 = ig.Graph(n=len(prot.atoms), edges=bonds)
     angles = set(get_angle_defs(graph1))
-    angles2 = set(guess_angles(ubqu.bonds))
+
+    angles2 = set(guess_angles(prot.bonds))
 
     assert angles == angles2
 
+@pytest.mark.parametrize('prot', [ubqu, mbp])
+def test_get_dihedral_defs(prot):
+    bonds = xl.guess_bonds(prot.atoms.positions, prot.atoms.types)
+    prot.add_bonds(bonds)
 
-def test_get_dihedral_defs():
+    graph1 = ig.Graph(n=len(prot.atoms), edges=bonds)
     dihedrals = set(get_dihedral_defs(graph1))
-    ubqu.add_angles(guess_angles(ubqu.bonds))
-    dihedrals2 = set(guess_dihedrals(ubqu.angles))
+
+    prot.add_angles(guess_angles(prot.bonds))
+    dihedrals2 = set(guess_dihedrals(prot.angles))
 
     assert dihedrals == dihedrals2
 
 
 def test_construction():
+    bonds = xl.guess_bonds(ubq.coords, ubq.atypes)
     top = Topology(ubq, bonds)
     for key, val in top.dihedrals_by_resnum.items():
 
@@ -40,6 +51,7 @@ def test_construction():
         chain = ubq.atoms[val[-1]].segid
         assert chain == key[0]
 
+
 def test_get_z_matrix():
     mbp_bonds = xl.guess_bonds(mbp.atoms.positions, mbp.atoms.names)
     top = Topology(mbp, mbp_bonds)
@@ -47,6 +59,7 @@ def test_get_z_matrix():
     test_idxs = top.get_zmatrix_dihedrals()
     ans_idxs = np.load('test_data/top_idxs.npy', allow_pickle=True)
     np.testing.assert_equal(test_idxs, ans_idxs)
+
 
 def test_dihedrals_by_atom():
     mbp_bonds = xl.guess_bonds(mbp.atoms.positions, mbp.atoms.names)

9d6af01953d4e9fb1c4eed7cf55ade8226b287bf
Maxx Tessmer <mhtessmer@gmail.com>
2024-04-25 13:00:48 -0700

    Use weights as fallback in save_io when KDE fails

--- src/chilife/io.py
+++ src/chilife/io.py
@@ -533,11 +533,15 @@ def write_labels(pdb_file: TextIO, *args: SpinLabel, KDE: bool = True, sorted: b
         spin_centers = np.atleast_2d(label.spin_centers)
 
         if KDE and len(spin_centers) > 5:
-            # Perform gaussian KDE to determine electron density
-            gkde = gaussian_kde(spin_centers.T, weights=label.weights)
+            try:
+                # Perform gaussian KDE to determine electron density
+                gkde = gaussian_kde(spin_centers.T, weights=label.weights)
 
-            # Map KDE density to pseudoatoms
-            vals = gkde.pdf(spin_centers.T)
+                # Map KDE density to pseudoatoms
+                vals = gkde.pdf(spin_centers.T)
+                
+            except:
+                vals = label.weights
 
         else:
             vals = label.weights

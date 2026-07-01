5e648b4d7b629ec12ff1ae74f9869a869d5f7842
Kyle Zhang <lzhang43@uw.edu>
2026-07-01 10:00:55 -0700

    Refactor partition_function to avoid energy recalculation
    Pull partition function calculation from existing energy calculation.

--- src/chilife/dRotamerEnsemble.py
+++ src/chilife/dRotamerEnsemble.py
@@ -722,11 +722,8 @@ class dRotamerEnsemble:
     @property
     def partition_function(self):
         """The partition function of the rotamer ensemble after reweighting."""
-        # Calculate external energies
-        energies = self.energy_func(self)
-        self.rot_clash_energy = energies
-        # Calculate total weights (combining internal and external)
-        self.weights, self.partition = scoring.reweight_rotamers(energies, self.temp, self.weights)
+        if self.partition == 1 and self.protein is not None:
+            self.evaluate()
         return self.partition
 
 def proc_sites(sites):
